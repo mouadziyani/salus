90c590c2c1d5095ec2fb00a63ef7d89fee4a6eb5
MOEUAED <ziyanimouad@gmail.com>
2026-03-24 11:01:57 +0100

    feat(routes): add symptoms, doctors, appointments, ai routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\SymptomController;
+use App\Http\Controllers\Api\DoctorController;
+use App\Http\Controllers\Api\AppointmentController;
+use App\Http\Controllers\Api\HealthAdviceController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -9,4 +13,23 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+
+    Route::get('/symptoms', [SymptomController::class, 'index']);
+    Route::post('/symptoms', [SymptomController::class, 'store']);
+    Route::get('/symptoms/{id}', [SymptomController::class, 'show']);
+    Route::put('/symptoms/{id}', [SymptomController::class, 'update']);
+    Route::delete('/symptoms/{id}', [SymptomController::class, 'destroy']);
+
+    Route::get('/appointments', [AppointmentController::class, 'index']);
+    Route::post('/appointments', [AppointmentController::class, 'store']);
+    Route::get('/appointments/{id}', [AppointmentController::class, 'show']);
+    Route::put('/appointments/{id}', [AppointmentController::class, 'update']);
+    Route::delete('/appointments/{id}', [AppointmentController::class, 'destroy']);
+
+    Route::get('/ai/health-advice', [HealthAdviceController::class, 'index']);
+    Route::post('/ai/health-advice', [HealthAdviceController::class, 'store']);
 });
+
+Route::get('/doctors', [DoctorController::class, 'index']);
+Route::get('/doctors/search', [DoctorController::class, 'search']);
+Route::get('/doctors/{id}', [DoctorController::class, 'show']);
